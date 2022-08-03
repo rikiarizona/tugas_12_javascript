8330a757143e304349a810477a562cc671d152dd
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:51:17 +0700

    TUGAS 12 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function namaBuah() {
+    var buah = ['pisang','jeruk','apel','mangga'];
+    console.log(buah);
+
+    buah.pop();
+    console.log(buah);
+
+    buah.shift();
+    return buah;
+}
+
+console.log(namaBuah());
